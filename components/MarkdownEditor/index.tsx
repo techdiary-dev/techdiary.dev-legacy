7cbf9a36460848ca9be0276331e178f96f882e56
King Rayhan <rayhan095@gmail.com>
2020-06-21 11:55:10 +0600

    TD-53: work in progress

---
+++ components/MarkdownEditor/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledMarkdownEditor } from "./styles";
+
+const MarkdownEditor = () => {
+  return (
+    <StyledMarkdownEditor>
+      <h1>Editor</h1>
+    </StyledMarkdownEditor>
+  );
+};
+
+export default MarkdownEditor;
--- pages/new.tsx
+++ pages/new.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import MainLayout from "components/Layout/MainLayout";
-import ArticleEditor from "components/Article/ArticleEditor";
+// import ArticleEditor from "components/Article/ArticleEditor";
 import PleaseLogin from "components/PleaseLogin";
 import HeadTag from "components/HeadTag";
+import MarkdownEditor from "components/MarkdownEditor";
 
 const NewArticlePage = () => {
   return (
     <MainLayout>
       <HeadTag title="নতুন ডায়েরি লিখুন" />
       <PleaseLogin>
-        <ArticleEditor />
+        <MarkdownEditor />
+        {/* <ArticleEditor /> */}
       </PleaseLogin>
     </MainLayout>
   );
