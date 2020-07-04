4b445330b9394402e4581094c568f9b4490b5a72
Shoaib Sharif <sharifshoaib@yahoo.com>
2020-07-04 00:00:41 -0700

    fixed the editor and bringing our plugins

--- components/MarkdownEditor/CodeMirror.tsx
+++ components/MarkdownEditor/CodeMirror.tsx
@@ -17,39 +17,19 @@ import "codemirror/addon/edit/closebrackets";
 import "codemirror/lib/codemirror";
 import "codemirror/mode/htmlmixed/htmlmixed";
 import { Controlled as CodeMirror } from "react-codemirror2";
-import crypto from "crypto";
 
 interface ICodeMirrorEditor {
   value: string;
-  mediaHandle?: (file: File) => Promise<String>;
-  onChanged: Function;
+  handleMedia?: (file: File) => void;
+  onChanged: (value: string) => void;
 }
 
 export const CodeMirrorEditor: React.FC<ICodeMirrorEditor> = (
   props: ICodeMirrorEditor
 ) => {
-  const [value, setValue] = useState(props.value);
-
-  useEffect(() => {
-    props.onChanged(value);
-  }, [value]);
-  useEffect(() => {
-    setValue(props.value);
-  }, [props.value]);
-
-  const handleMedia = async (file: File) => {
-    const imageUUID = crypto.randomBytes(12).toString("hex");
-    const str = `[Uploading...](${imageUUID})`;
-    setValue(`${value}\n${str}`);
-
-    console.log(str);
-
-    const url = await props.mediaHandle(file);
-    if (url) {
-      setValue(`${value.replace(str, `[image_alt_text](${url})`)}\n`);
-    }
+  const handleMedia = (file: File) => {
+    if (typeof props.handleMedia !== "undefined") props.handleMedia(file);
   };
-
   const handleDrop = (editor: CodeMirror.Editor, event: DragEvent) => {
     if (!event.dataTransfer) return;
 
@@ -70,31 +50,34 @@ export const CodeMirrorEditor: React.FC<ICodeMirrorEditor> = (
   };
   return (
     <CodeMirror
-      value={value}
+      value={props.value}
       options={{
         mode: "yaml-frontmatter",
         theme: "solarized light",
         indentUnit: 2,
         indentWithTabs: false,
         autoCloseBrackets: true,
-        // extraKeys: {
-        //   Enter: (editor: CodeMirror.Editor) => {
-        //     editor.execCommand("newlineAndIndentContinueMarkdownList");
-        //   },
-        //   Tab: (editor: CodeMirror.Editor) => {
-        //     editor.execCommand("autoIndentMarkdownList");
-        //   },
-        //   "Shift-Tab": (editor: CodeMirror.Editor) => {
-        //     editor.execCommand("autoUnindentMarkdownList");
-        //   },
-        // },
+        extraKeys: {
+          Enter: (editor: CodeMirror.Editor) => {
+            editor.execCommand("newlineAndIndentContinueMarkdownList");
+          },
+          Tab: (editor: CodeMirror.Editor) => {
+            editor.execCommand("autoIndentMarkdownList");
+          },
+          "Shift-Tab": (editor: CodeMirror.Editor) => {
+            editor.execCommand("autoUnindentMarkdownList");
+          },
+        },
         lineWrapping: true,
         addModeClass: true,
       }}
-      // editorDidMount={(editor: CodeMirror.Editor) => {
-      //   new MTableEditor(editor);
-      // }}
-      onBeforeChange={(editor, data, value) => setValue(value)}
+      editorDidMount={(editor: CodeMirror.Editor) => {
+        new MTableEditor(editor);
+      }}
+      onBeforeChange={(editor, data, value) => {
+        console.log("codemirror value", value);
+        props.onChanged(value);
+      }}
       onDrop={handleDrop}
       onPaste={handlePaste}
     />
