5914a3252df5eb7cbb4774cad10efff7b363021f
King Rayhan <rayhan095@gmail.com>
2020-07-04 15:02:17 +0600

    improve header on mobile

--- components/Layout/Navbar/Logo/index.tsx
+++ components/Layout/Navbar/Logo/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import { LogoStyle } from './styles'
-import Link from 'next/link'
+import React from "react";
+import { LogoStyle } from "./styles";
+import Link from "next/link";
+import TDLogo from "../../../../public/logos/logo-dark.svg";
 
 const Logo: React.FC = () => {
-	return (
-		<LogoStyle>
-			<Link href="/">
-				<a>
-					<img src="/logos/logo.png" alt="Techdiary.dev logo" />
-				</a>
-			</Link>
-		</LogoStyle>
-	)
-}
+  return (
+    <LogoStyle>
+      <Link href="/">
+        <a>
+          <TDLogo />
+        </a>
+      </Link>
+    </LogoStyle>
+  );
+};
 
-export default Logo
+export default Logo;
--- components/Layout/Navbar/Search/styles.ts
+++ components/Layout/Navbar/Search/styles.ts
@@ -1,45 +1,44 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import deviceScreens from "styles/DEVICES";
 
 export const StyledSearch = styled.div`
-	width: 50%;
-	max-width: 600px;
-	margin: auto;
-	text-align: center;
-	position: relative;
-	margin: 0 15px;
+  max-width: 600px;
+  margin: auto;
+  text-align: center;
+  position: relative;
+  margin: 0 15px;
+  width: 50%;
 
-	.search-icon {
-		position: absolute;
-		right: 10px;
-		top: 50%;
-		height: 20px;
-		width: 20px;
-		transform: translateY(-50%);
-		color: ${({ theme }) => theme.darkGrey};
-	}
+  @media all and (max-width: ${deviceScreens.MOBILE_SCREEN}) {
+    width: 80%;
+  }
 
-	input {
-		border: none;
-		padding: 12px 4rem 12px 15px;
-		border-radius: 5px;
-		background-color: #eeeeee;
-		font-size: 1.5rem;
-		font-family: 'Kohinoor Bangla';
-		flex-grow: 1;
-		width: 100%;
+  .search-icon {
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    height: 20px;
+    width: 20px;
+    transform: translateY(-50%);
+    color: ${({ theme }) => theme.darkGrey};
+  }
 
-		transition: 500ms;
+  input {
+    border: none;
+    padding: 12px 4rem 12px 15px;
+    border-radius: 5px;
+    background-color: #eeeeee;
+    font-size: 1.5rem;
+    font-family: "Kohinoor Bangla";
+    flex-grow: 1;
+    width: 100%;
 
-		/* &:hover {
-			background-color: #efecec;
-			box-shadow: 0 0px 3px rgba(0, 0, 0, 0.16);
-			cursor: pointer;
-		} */
+    transition: 500ms;
 
-		&:focus {
-			outline: none;
-			box-shadow: 0 0 0;
-			cursor: initial;
-		}
-	}
-`
+    &:focus {
+      outline: none;
+      box-shadow: 0 0 0;
+      cursor: initial;
+    }
+  }
+`;
