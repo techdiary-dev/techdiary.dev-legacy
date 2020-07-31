d11e21c63bea9bd0dc6cbf6373af4ef639d3a8eb
Md Raihan <rayhan095@gmail.com>
2020-07-31 23:28:24 +0600

    Update LeftSidebar.tsx

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -4,10 +4,7 @@ import FeaturedTag from "./FeaturedTag";
 const LeftSidebar = () => {
   return (
     <>
-      <FeaturedTag
-        primaryTag="js-array-challenge"
-        tags={["js-array-challenge", "challenge"]}
-      />
+      <FeaturedTag primaryTag="js-array-challenge" limit={8}/>
       <FeaturedTag primaryTag="php" />
     </>
   );
